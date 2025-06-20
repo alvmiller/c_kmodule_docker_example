8be72da3121fca09e138416a1e2881158a0d27bf
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-06-21 00:19:57 +0300

    Update script_docker_host.sh

--- src/script_docker_host.sh
+++ src/script_docker_host.sh
@@ -3,7 +3,7 @@
 clear
 reset
 
-echo "Host Part..."
+echo "Starting..."
 sleep 1
 echo -e "\n"
 
@@ -16,17 +16,11 @@ gcc -o client client.c
 sleep 1
 echo -e "\n"
 
+gcc -o client_host -DHOST_DEV_PROC_DRV client.c
+sleep 1
+echo -e "\n"
+
 xxd -i example_dev.ko >example_dev.ko.h
 gcc -o init_drv_client init_drv_client.c
 sleep 1
 echo -e "\n"
-
-#sudo docker run \
-#	-v /dev/:/root/dev_ex/ \
-#	...
-sudo docker run \
-	-it \
-	--privileged --cap-add SYS_MODULE \
-	--hostname docker \
-	--mount "type=bind,src=$PWD,dst=/root" \
-	ubuntu
